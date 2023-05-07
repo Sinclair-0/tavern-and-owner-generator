8dfb6658bfde5f850b9bf1aa174ea544e01ea6b6
Cameron Boyd <csinclairboyd@gmail.com>
2023-05-06 23:00:52 -0500

    More grammar tweaks and added a few more options to bodyHeight, bodyWeight, and quirks.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ var nouns = ["Hero", "Mystic", "Mage", "Miner", "Farmer", "Poet", "Courier", "Co
 var container = document.getElementById("Tavern_Name");
 var randomnumber1;
 var randomnumber2;
-function generateRichName()
-{
+function generateRichName() {
     var name;
     var richName = ["Elite", "Poised", "Gilded", "Golden", "Jeweled", "Ruby", "Jade", "Garnet", "Amethyst", "Crystal", "Sapphire", "Diamond", "Silver", "Opulent", "Ornate", "Splendid", "Graceful", "Flowery", "Luxurious", "Emerald", "Posh", "Extravagant", "Grand", "Lush", "Lavish", "Pampered", "Luscious", "Majestic", "Grandiose", "Stately", "Lofty", "Gallant", "Brilliant", "Ritzy", "Classy", "Refined", "Kingly", "King's", "Queenly", "Queen's", "Lord's", "Lady's", "Scholar's", "Scholarly", "Elaborate", "Elegant", "Dignified", "Regal", "Royal", "Noble", "Noble's", "Magnificent", "Deluxe", "Illustrious", "Glorious", "Esteemed", "Exalted", "Prestigous", "Fine", "Fancy", "Adorned", "Sparkling", "Dazzling", "Prosperous", "Resplendent", "Luminous", "Vivid", "Radiant", "Starry", "Sunny", "Twilight", "Twilit", "Moonlit", "Sunlit", "Bright", "Starlit", "Silvery", "Immaculate", "Knight's", "Crowned", "Valiant", "Heroic", "Mystical", "Shimmering", "Arcane", "Marvelous", "Frosted", "Sophisticated", "Ethereal", "Mature", "Exultant", "Flawless", "Silken", "Dapper", "Tranquil"];
     randomnumber1 = Math.floor(Math.random() * (richName.length));
@@ -11,8 +10,7 @@ function generateRichName()
     name = 'The ' + richName[randomnumber1] + ' ' + nouns[randomnumber2];
     container.innerHTML = name;
 }
-function generateMidName()
-{
+function generateMidName() {
     var name;
     var midName = ["Stubborn", "Rainy", "Thirsty", "Crystal", "Steel", "Zippy", "Dizzy", "Tranquil", "Soft", "Warm", "Zealous", "Quaint", "Mature", "Novel", "Marvelous", "Sleepy", "Sleeping", "Slumbering", "Silver", "Gilded", "Twilight", "Copper", "Bashful", "Tapped", "Brazen", "Brass", "Valiant", "Chilly", "Heroic", "Bold", "Stalwart", "Dreaming", "Blind", "Misty", "Mystical", "Rustic", "Arcane", "Sly", "Wise", "Spectral", "Grizzled", "Grizzly", "Smoky", "Onyx", "Frosty", "Wild", "Raging", "Plain", "Fluttering", "Rough", "Parched", "Peppy", "Ethereal", "Fumbling", "Jittery", "Clumsy", "Lively", "Exultant", "Nimble", "Metal", "Metallic", "Whispering", "Shiny", "Resolute", "Humble", "Lowly", "Calm", "Bizzare", "Sassy", "Dashing", "Testy", "Alluring", "Fierce", "Firey", "Sturdy", "Hungry", "Plucky", "Tender", "Sneaky", "Poised", "Stumbling", "Iron", "Spotted"];
     randomnumber1 = Math.floor(Math.random() * (midName.length));
@@ -20,8 +18,7 @@ function generateMidName()
     name = 'The ' + midName[randomnumber1] + ' ' + nouns[randomnumber2];
     container.innerHTML = name;
 }
-function generatePoorName()
-{
+function generatePoorName() {
     var name;
     var poorName = ["Stubborn", "Butchered", "Rainy", "Stormy", "Sloven", "Stumbling", "Firey", "Craven", "Condemned", "Testy", "Callous", "Lonely", "Lone", "Rebellious", "Rabid", "Jagged", "Lowly", "Nimble", "Vulgar", "Metal", "Metallic", "Whispering", "Sleazy", "Sorid", "Spiteful", "Bent", "Cheap", "Plain", "Disheveled", "Bitter", "Raging", "Wild", "Onyx", "Obsidian", "Twilight", "Moonlit", "Sunlit", "Drunken", "Drunk", "Scarred", "Smoked", "Smoky", "Shrouded", "Enshrouded", "Brazen", "Copper", "Brass", "Silver", "Frigid", "Cloaked", "Thief's", "Poison", "Dead", "Jailed", "Chained", "Shackled", "Rusty", "Rusted", "Shadowy", "Sullen", "Ashen", "Crooked", "Crook's", "Blooded", "Bloody", "Cross", "Blind", "Dim", "Bold", "Grizzled", "Grizzly", "Sleepy", "Sleeping", "Crimson", "Hazy", "Dark", "Taboo", "Murky", "Misty", "Somber", "Wicked", "Vile", "Dour", "Bleak", "Occult", "Veiled", "Drenched", "Sly", "Grave", "Foggy", "Vexed", "Thorned", "Thorny", "Drowned", "Stained", "Lead", "Seedy", "Stingy", "Lying", "Parched", "Greedy", "Thirsty", "Prickly", "Troubled", "Hissing", "Limping", "Hungry", "Dead Man's", "Rough", "Dizzy", "Steel", "Iron"];
     randomnumber1 = Math.floor(Math.random() * (poorName.length));
@@ -29,8 +26,7 @@ function generatePoorName()
     name = 'The ' + poorName[randomnumber1] + ' ' + nouns[randomnumber2];
     container.innerHTML = name;
 }
-function surprise()
-{
+function surprise() {
     var number = Math.floor(Math.random() * 3);
     if(number = 0)
     {
@@ -51,10 +47,10 @@ function surprise()
 }
 function owner() {
     var owner;
-    var bodyHeight = ["short", "tall", "an average height"];
-    var bodyType = ["portly", "lean", "bulky", "scrawny", "an average weight"];
+    var bodyHeight = ["short", "very short", "very tall", "tall", "an average height"];
+    var bodyType = ["portly", "lean", "bulky", "chubby", "scrawny", "an average weight"];
     var personality = ["calm", "seductive", "loud", "quiet", "meek", "angry", "rude", "solemn", "dorky", "bossy", "smug", "stubborn", "serious", "jokey", "cheery", "kind", "bored", "apathetic", "lively", "snobby", "humble", "friendly"];
-    var quirks = [" speaks with a slight stutter.", " has an embarrassing tattoo.", " only wears glass jewelry.", " doesn't understand sarcasm.", " is sarcastic.", " is sober.", " has a slight limp.", " always has a torn left sleeve.", " loves gossip.", " a werewolf.", " has an uncanny memory.", " was an orphan.", " has a kid", " knows a noble, personally.", " is superstitious", " frequently cleans their glasses", " has an evil (sounding) laugh.", " uses slang, incorrectly.", " hums frequently.", " holds grudges.", " has a weakness for sweets.", " dislikes sweets.", " collect rocks.", " collect trinkets.", " is an avid reader.", " dislikes reading.", " has vocal fry.", " quotes things incorrectly.", " laughs at their own jokes.", " have a fear of spiders.", " have a fear of cats.", " have a fear of dogs.", " is afraid of the dark.", " has a monotone voice.", " dislikes eating in front of others.", " believes in a wild conspiracy theory.", " will not eat food they didn't make.", " will not drink something they didn't pour.", " loves riddles.", " speaks in rhyme.", " loves puns.", " doesn't understand puns.", " talks with their hands.", " is a minimalist.", " is a maximalist.", " always misprounouces a certain word.", " doesn't curse.", " loves spicy foods.", " loves card tricks.", " loves doing card tricks.", " is a gambler.", " does not like gambling.", " whistles to themselves", " has a terrible sense of direction.", " pretends to be stupid.", " pretends to be smart.", " loves stormy weather.", " is afraid of thunder.", " mutters small onomatopoeia noises to themselves while working.", " is vain.", " frequently checking/fixing their appearance.", " eavesdrops.", " smells pleasant.", " has a soft laugh.", " has a loud laugh.", " has aphantasia.", ];
+    var quirks = [" speak with a slight stutter.", " have an embarrassing tattoo.", " only wear glass jewelry.", " doesn't understand sarcasm.", " sarcastic.", " sober.", " have a slight limp.", " always have a torn left sleeve.", " love to gossip.", " a werewolf.", " have an uncanny memory.", " was an orphan.", " have a kid", " know a noble, personally.", " very superstitious", " frequently clean their glasses", " have an evil sounding laugh.", " use slang incorrectly.", " hum to themself.", " hold grudges.", " have a weakness for sweets.", " dislike sweets.", " collect rocks.", " collect trinkets.", " an avid reader.", " dislike reading.", " have vocal fry.", " quotes things incorrectly.", " laugh at their own jokes.", " have a fear of spiders.", " have a fear of cats.", " have a fear of dogs.", " afraid of the dark.", " have a monotone voice.", " dislike eating in front of others.", " believe in a wild conspiracy theory.", " will not eat food they didn't make.", " will not drink something they didn't pour.", " love riddles.", " speak in rhymes.", " love puns.", " doesn't understand puns.", " talk with their hands.", " a minimalist.", " a maximalist.", " always misprounouces a certain word.", " doesn't curse.", " love spicy food.", " love card tricks.", " love doing card tricks.", " a gambler.", " does not like gambling.", " whistle to themself", " have a terrible sense of direction.", " pretend to be stupid.", " pretend to be smart.", " love stormy weather.", " afraid of thunder.", " mutter to themself while working.", " vain.", " frequently check their appearance.", " an eavesdropper.", " smell pleasant.", " have a soft laugh.", " have a loud laugh.", " have aphantasia.", " love coffee.", " have a love for stray animals.", " always sneezes in twos.", " a terrible liar.", " never lies.", " a compulsive liar.", " always a little early.", " always a little late.", " a bit absent-minded.", " always using metaphors.", " a slow talker.", " a fast talker."];
     var randomnumber3;
     var randomnumber4;
     randomnumber1 = Math.floor(Math.random() * (bodyHeight.length));
@@ -64,10 +60,8 @@ function owner() {
 
     owner = 'The owner is ' + bodyHeight[randomnumber1] + ' and ' + bodyType[randomnumber2] + '. They are ' + personality[randomnumber3] + ' and ' + quirks[randomnumber4];
     document.getElementById("Owner_Name").innerHTML = owner;
-
-
-
 }
+
 document.getElementById("Rich").addEventListener("click", generateRichName, false);
 document.getElementById("Middle").addEventListener("click", generateMidName, false);
 document.getElementById("Poor").addEventListener("click", generatePoorName, false);
